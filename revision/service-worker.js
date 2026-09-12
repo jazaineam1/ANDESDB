a994e8959f7409d47ee2396eed6bcd39f4a5ae36
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-12 11:52:51 -0500

    Refresh LMS request workflow cache

--- revision/service-worker.js
+++ revision/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION='andesdb-lms-20260912-v13';
+const VERSION='andesdb-lms-20260912-v14';
 const CORE=`${VERSION}-core`,RUNTIME=`${VERSION}-runtime`,BASE=new URL('./',self.location.href).pathname;
 const ESSENTIAL=[
   './','./portal.html','./index.html','./learning-hub.html','./lab.html','./teacher-dashboard.html','./manifest.webmanifest',
