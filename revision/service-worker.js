b198989f8d8c6ddea5eef9b38b6f0c77a41c0359
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-12 12:04:54 -0500

    Refresh zero-cost access flow cache

--- revision/service-worker.js
+++ revision/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION='andesdb-lms-20260912-v14';
+const VERSION='andesdb-lms-20260912-v15';
 const CORE=`${VERSION}-core`,RUNTIME=`${VERSION}-runtime`,BASE=new URL('./',self.location.href).pathname;
 const ESSENTIAL=[
   './','./portal.html','./index.html','./learning-hub.html','./lab.html','./teacher-dashboard.html','./manifest.webmanifest',
