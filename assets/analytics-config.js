464e411fbd258f911c2e5042a04f949fc255c72c
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-12 15:50:12 -0500

    Add GA4 config for public ANDESDB site

---
+++ assets/analytics-config.js
@@ -0,0 +1,10 @@
+window.ANDES_PUBLIC_ANALYTICS_CONFIG = Object.freeze({
+  enabled: true,
+  ga4MeasurementId: 'G-Z5YG0TNP8J',
+  sendPageViews: true,
+  trackSlides: true,
+  trackOutboundLinks: true,
+  allowGoogleSignals: false,
+  allowAdPersonalization: false,
+  privacyMode: 'no-pii'
+});
