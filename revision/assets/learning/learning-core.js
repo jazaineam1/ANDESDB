0a9fd26befb04ddbfd6cf55ec02dfd877dbb95cf
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-12 21:41:55 -0500

    Carga contrato narrativo pregunta-respuesta

--- revision/assets/learning/learning-core.js
+++ revision/assets/learning/learning-core.js
@@ -11,6 +11,7 @@
   load(new URL('learning-tracker.js?v=20260912-lms', dir).href);
   if(document.querySelector('.slide')){
     load(new URL('presentation-story-v1.js?v=20260912-story1', dir).href);
+    load(new URL('presentation-story-v2-patch.js?v=20260912-story2', dir).href);
     if(/sesion[-_\s]*15/i.test(location.pathname+' '+document.title))load(new URL('presentation-story-s15-patch.js?v=20260912-story1', dir).href);
   }
 })();
