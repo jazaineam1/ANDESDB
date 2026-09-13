65d46acc8760be23169c257938430081288a39ff
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-12 22:29:18 -0500

    Alinea S13-S16 con materiales adicionales

--- revision/assets/learning/learning-core.js
+++ revision/assets/learning/learning-core.js
@@ -13,7 +13,10 @@
     load(new URL('presentation-story-v1.js?v=20260912-story1', dir).href);
     load(new URL('presentation-story-v2-patch.js?v=20260912-story2', dir).href);
     load(new URL('presentation-story-v3-polish.js?v=20260912-story3', dir).href);
-    if(/sesion[-_\s]*1[3-6]/i.test(location.pathname+' '+document.title))load(new URL('presentation-selfcontained-v1.js?v=20260913-self1', dir).href);
+    if(/sesion[-_\s]*1[3-6]/i.test(location.pathname+' '+document.title)){
+      load(new URL('presentation-selfcontained-v1.js?v=20260913-self1', dir).href);
+      load(new URL('presentation-material-alignment-v1.js?v=20260913-align1', dir).href);
+    }
     if(/sesion[-_\s]*15/i.test(location.pathname+' '+document.title))load(new URL('presentation-story-s15-patch.js?v=20260912-story1', dir).href);
   }
 })();
