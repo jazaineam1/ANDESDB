f05186b825a81a425f3e41857518c9baa3be60df
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-12 22:57:17 -0500

    Fuerza contexto actualizado en S13-S16

--- revision/assets/learning/learning-core.js
+++ revision/assets/learning/learning-core.js
@@ -15,7 +15,7 @@
     load(new URL('presentation-story-v3-polish.js?v=20260912-story3', dir).href);
     if(/sesion[-_\s]*1[3-6]/i.test(location.pathname+' '+document.title)){
       load(new URL('presentation-selfcontained-v1.js?v=20260913-self1', dir).href);
-      load(new URL('presentation-material-alignment-v1.js?v=20260913-align1', dir).href);
+      load(new URL('presentation-material-alignment-v1.js?v=20260913-align2', dir).href);
     }
     if(/sesion[-_\s]*15/i.test(location.pathname+' '+document.title))load(new URL('presentation-story-s15-patch.js?v=20260912-story1', dir).href);
   }
