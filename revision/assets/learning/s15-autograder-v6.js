47dd21af87ec52cabd1ffa7364d92fb5de5b59a5
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-18 17:12:00 -0500

    S15 v7: retira grader cliente v6 con claves antiguas

--- revision/assets/learning/s15-autograder-v6.js
+++ revision/assets/learning/s15-autograder-v6.js
@@ -1,1 +1,1 @@
-(()=>{'use strict';const self=document.currentScript;const core=document.createElement('script');core.src=new URL('s15-autograder-v6b.js?v=s15v6b',self.src).href;core.defer=false;core.onload=()=>{const az=document.createElement('script');az.src=new URL('s15-azure-transfer-v1.js?v=20260916a',self.src).href;az.defer=false;core.after(az)};self.after(core);})();
+(()=>{'use strict';console.warn('S15 v6 fue retirada; usa evaluador-s15-v7.html.');})();
